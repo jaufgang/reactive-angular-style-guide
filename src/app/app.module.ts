549c5e90fb312af7e43e6d4125a3a4b4e7debca2
Sumit Arora <er.sumitarora@gmail.com>
2021-04-15 17:30:54 -0400

    removing contentful

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { HIGHLIGHT_OPTIONS, HighlightModule } from 'ngx-highlightjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxContentfulRichTextModule } from 'ngx-contentful-rich-text';
 import { MarkdownToHtmlModule } from 'markdown-to-html-pipe';
 
 import { PrettierPipe } from './prettier.pipe';
@@ -14,14 +13,14 @@ import { DiscussionComponent } from './discussion/discussion.component';
 import { RulesComponent } from './rules/rules.component';
 import { SamplesComponent } from './samples/samples.component';
 import { TitleComponent } from './title/title.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, PrettierPipe, DiscussionComponent, RulesComponent, SamplesComponent, TitleComponent],
+  declarations: [AppComponent, PrettierPipe, DiscussionComponent, RulesComponent, SamplesComponent, TitleComponent, NavComponent],
   imports: [
     BrowserModule,
     HighlightModule,
     BrowserAnimationsModule,
-    NgxContentfulRichTextModule,
     MarkdownToHtmlModule,
   ],
   providers: [
